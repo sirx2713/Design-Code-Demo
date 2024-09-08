5c4033d011bc4b22b4d09dacf4532069d024ee59
iSAD <bradx2713@gmail.com>
2024-09-08 23:32:06 +0200

    Initial Commit

---
+++ Design Code Demo/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  Design Code Demo
+//
+//  Created by Chris Ching on 2021-07-28.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+    
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Design Code Demo/Utilities/Fonts.swift
@@ -0,0 +1,24 @@
+//
+//  Fonts.swift
+//  Design Code Demo
+//
+//  Created by Chris Ching on 2021-07-28.
+//
+
+import Foundation
+import SwiftUI
+
+/*
+ SF Pro Display
+ - SFProDisplay-Regular
+ - SFProDisplay-Semibold
+ - SFProDisplay-Bold
+ SF Pro Text
+ - SFProText-Regular
+ */
+
+extension Font {
+    
+    static var title = Font.custom("SFProDisplay-Bold", size: 28)
+    
+}
